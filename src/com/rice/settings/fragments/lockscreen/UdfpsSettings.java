ca1f05d179392205adf0dc84efad7fef839cf26f
SagarMakhar <sagarmakhar@gmail.com>
2022-05-15 11:29:48 +0800

    rice: Make Udfps haptic feedback optional [2/2]
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>

--- src/com/rice/settings/fragments/lockscreen/UdfpsSettings.java
+++ src/com/rice/settings/fragments/lockscreen/UdfpsSettings.java
@@ -43,13 +43,15 @@ import com.android.internal.util.crdroid.Utils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class UdfpsSettings extends SettingsPreferenceFragment {
+public class UdfpsSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String UDFPS_ANIM_PREVIEW = "udfps_recognizing_animation_preview";
     private static final String SCREEN_OFF_UDFPS = "screen_off_udfps";
+    private static final String UDFPS_HAPTIC_FEEDBACK = "udfps_haptic_feedback";
 
     private Preference mUdfpsAnimPreview;
     private Preference mScreenOffUdfps;
+    private Preference mUdfpsHapticFeedback;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,6 +64,7 @@ public class UdfpsSettings extends SettingsPreferenceFragment {
         final boolean udfpsResPkgInstalled = Utils.isPackageInstalled(getContext(),
                 "com.crdroid.udfps.animations");
         mUdfpsAnimPreview = findPreference(UDFPS_ANIM_PREVIEW);
+        mUdfpsHapticFeedback = findPreference(UDFPS_HAPTIC_FEEDBACK);
         if (!udfpsResPkgInstalled) {
             prefSet.removePreference(mUdfpsAnimPreview);
         }
@@ -71,6 +74,18 @@ public class UdfpsSettings extends SettingsPreferenceFragment {
                 com.android.internal.R.bool.config_supportScreenOffUdfps);
         if (!mScreenOffUdfpsAvailable)
             prefSet.removePreference(mScreenOffUdfps);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    	final ContentResolver resolver = getActivity().getContentResolver();
+	if (preference == mUdfpsHapticFeedback) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.UDFPS_HAPTIC_FEEDBACK, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     public static void reset(Context mContext) {
